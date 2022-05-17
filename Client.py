5eae070b62fc4bb2a768398ee6ec69d5e26dd970
Ahmedmma72 <ahmedmma561@gmail.com>
2022-05-17 04:11:58 +0200

    final touches on server and client

--- Client.py
+++ Client.py
@@ -1,42 +1,42 @@
+from http import client
 import socket
 import pickle
 import Algorithms
+import utils
 
 #setup socket
 HeaderSize = 10
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 3000))
+client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((socket.gethostname(), 3000)) #connects to the server
 
 
+
+
+#setup algorithm
+primeArray = utils.read_data('primes.txt')
+(p,q) = Algorithms.generate_two_prime_numbers(primeArray)
+(e,d,n) = Algorithms.RSA_key_generator(p,q)        
+
+#first send public key to server
+publicKey = (e,n)
+publicKey = pickle.dumps(publicKey) 
+publicKey = bytes(f'{len(publicKey):<{HeaderSize}}', "utf-8") + publicKey # :< means left align
+client_socket.send(publicKey)
+
+#variables for recieving messages
 full_msg = b''
 new_msg  = True
 msg_len = 0
 
-#receive public key from server
-pkRecieved = False
-while not pkRecieved:
-    msg = s.recv(16)
-    if new_msg:
-        msg_len = int(msg[:HeaderSize])
-        new_msg = False  
-    full_msg += msg
-    if len(full_msg)-HeaderSize == msg_len:
-        msg = pickle.loads(full_msg[HeaderSize:])
-        (e,n) = msg
-        new_msg = True
-        full_msg = b''
-        pkRecieved = True
-
-print("Public key recieved")
 while True:
-    msg = s.recv(16)
+    msg = client_socket.recv(16)
     if new_msg:
         msg_len = int(msg[:HeaderSize])
         new_msg = False  
     full_msg += msg
     if len(full_msg)-HeaderSize == msg_len:
         msg = pickle.loads(full_msg[HeaderSize:])
-        msg = Algorithms.Decrypt(msg,n,e)
+        msg = Algorithms.Decrypt(msg,n,d)
         print("recieved message: ",msg)
         new_msg = True
         full_msg = b''
--- Server.py
+++ Server.py
@@ -4,27 +4,38 @@ import Algorithms
 import utils
 
 #setup socket
-HeaderSize = 10
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind((socket.gethostname(), 3000))
-serverSocket.listen(1)
+HeaderSize = 10 #so the length of largest message is 99..9 10 times (pretty big i guess)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #fist param is address family, second is socket type
+serverSocket.bind((socket.gethostname(), 3000)) #binds the socket to the port
+serverSocket.listen(1) #listens for 1 connection
 
-#setup algorithm
-primeArray = utils.read_data('primes.txt')
-(p,q) = Algorithms.generate_two_prime_numbers(primeArray)
-(e,d,n) = Algorithms.RSA_key_generator(p,q)
+#varaibles to recieve public key
+full_msg = b''
+new_msg  = True
+msg_len = 0
+e = 0
+n = 0
 
 while True:
     clientSocket, address = serverSocket.accept()
-    #first send public key to client
-    publicKey = (e,n)
-    publicKey = pickle.dumps(publicKey)
-    publicKey = bytes(f'{len(publicKey):<{HeaderSize}}', "utf-8") + publicKey
-    clientSocket.send(publicKey)
+    #reciver public key from client
+    pkRecieved = False
+    while not pkRecieved:
+        msg = clientSocket.recv(HeaderSize)
+        if new_msg:
+            msg_len = int(msg[:HeaderSize])
+            new_msg = False
+        full_msg += msg
+        if len(full_msg)-HeaderSize == msg_len:
+            msg = pickle.loads(full_msg[HeaderSize:])
+            (e,n) = msg
+            pkRecieved = True
+
+    print("Public key recieved :" , e,n)    
     while True:
         msg = input("Enter a message: ")
-        msg = Algorithms.Encrypt(msg,n,d)
-        msg = pickle.dumps(msg)
+        msg = Algorithms.Encrypt(msg,n,e)
+        msg = pickle.dumps(msg) #converts to bytes
         msg = bytes(f"{len(msg):<{HeaderSize}}", 'utf-8') + msg
         clientSocket.send(msg)
 
