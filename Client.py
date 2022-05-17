f0d24a140af71a7baed9ab114c5170d97af6b8c6
Ahmedmma72 <ahmedmma561@gmail.com>
2022-05-17 21:27:08 +0200

    modes of operation added

--- Client.py
+++ Client.py
@@ -9,13 +9,35 @@ HeaderSize = 10
 client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((socket.gethostname(), 3000)) #connects to the server
 
-
-
+#choose mode of operation
+print("Welcome to the client!")
+print("1) Normal (Keys are auto generated)\n2) testing(You get to insert key parameters)\n")
+mode = input("Enter Your Choice: ")
+while(mode != "1" and mode != "2"):
+    print("Invalid Choice!")
+    mode = input("Enter Your Choice: ")
 
 #setup algorithm
-primeArray = utils.read_data('primes.txt')
-(p,q) = Algorithms.generate_two_prime_numbers(primeArray)
-(e,d,n) = Algorithms.RSA_key_generator(p,q)        
+if(mode == "1"):
+    primeArray = utils.read_data('primes.txt')
+    (p,q) = Algorithms.generate_two_prime_numbers(primeArray)
+    (e,d,n) = Algorithms.RSA_key_generator(p,q)
+else:
+    p = int(input("Enter p: "))
+    while(utils.isPrime(p) == False):
+        print("P must be PRIME!")
+        p = int(input("Enter p: "))
+    q = int(input("Enter q: "))
+    while(utils.isPrime(q) == False or p*q < 256):
+        print("q must be PRIME and p*q must be greater than 255!")
+        q = int(input("Enter q: "))
+    n = p*q    
+    e = int(input("Enter e: "))
+    while(utils.GCD(e,(p-1)*(q-1)) != 1 or e < 1 or e > (p-1)*(q-1)):
+        print("e must be greater than 1 ,smaller than (p-1)*(q-1) ,and coprime to (p-1)*(q-1)!")
+        e = int(input("Enter e: "))
+    d = utils.InverseModulo(e,(p-1)*(q-1))    
+          
 
 #first send public key to server
 publicKey = (e,n)
@@ -23,6 +45,7 @@ publicKey = pickle.dumps(publicKey)
 publicKey = bytes(f'{len(publicKey):<{HeaderSize}}', "utf-8") + publicKey # :< means left align
 client_socket.send(publicKey)
 
+print("ready to recieve message")
 #variables for recieving messages
 full_msg = b''
 new_msg  = True
--- utils.py
+++ utils.py
@@ -71,3 +71,14 @@ def getNumberOfChars(key):
         noChar += 1
         key = key // 256
     return noChar
+
+#function to check if a number is prime
+def isPrime(n):
+    if n == 2:
+        return True
+    if n % 2 == 0 or n <= 1:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):   # only odd numbers
+        if n % i == 0:
+            return False
+    return True
